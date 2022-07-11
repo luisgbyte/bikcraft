632ba432290296e20b7cfd62d0a14614b4559757
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-07-11 15:47:14 -0300

    add menu ativo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(links) {
+  const url = location.href;
+  const href = links.href;
+
+  if (url.includes(href)) {
+    links.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
