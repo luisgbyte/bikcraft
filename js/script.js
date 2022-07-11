1cee1aea70bc852c4a1d732a4eb0a0da54b82c62
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-07-11 16:18:21 -0300

    add selecao por url parametros

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(links) {
@@ -10,3 +11,15 @@ function ativarLink(links) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
